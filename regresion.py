85d839715345daa909abb210434ff880989dd66f
Clever Gomez <cleverg2004@gmail.com>
2024-04-21 16:07:38 -0400

    cambiostexto
    problema de texto resolviendo

--- regresion.py
+++ regresion.py
@@ -1,18 +1,29 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
+import os
 
-# Paso 1: Carga y Preparación de los Datos
+# Asegúrate de cambiar al directorio correcto
+os.chdir(r'C:\Users\cleve\Documents\GitHub\regresion_lineal')
+
+# Verificar que estamos en el directorio correcto y listar archivos
+print("Directorio de trabajo actual:", os.getcwd())
+print("Archivos en el directorio actual:", os.listdir('.'))
+
+# Asegúrate de que 'tratamiento.csv' está en la lista anterior
 df = pd.read_csv('tratamiento.csv')
+
+print(df.head())  # Muestra las primeras 5 filas del DataFrame
+print(df.describe())  # Proporciona un resumen estadístico de los datos, útil para verificación rápida
 
 # Asumiendo que tienes una columna para predecir que podría ser algo como 'ResultadoTratamiento'
 datos_entrenamiento = df[['Tipo_cx', 'Tratamiento', 'Dieta_recomendada']].values
-etiquetas_entrenamiento = df['ResultadoTratamiento'].values
+#etiquetas_entrenamiento = df['ResultadoTratamiento'].values
 
 
 # Convertimos los datos a tipos flotantes
 datos_entrenamiento = datos_entrenamiento.astype(float)
-etiquetas_entrenamiento = etiquetas_entrenamiento.astype(float)
+#etiquetas_entrenamiento = etiquetas_entrenamiento.astype(float)
 
 # Normalización de los datos
 media = np.mean(datos_entrenamiento, axis=0)
@@ -26,13 +37,13 @@ modelo = tf.keras.Sequential([
 ])
 
 # Configuración del modelo
-modelo.compile(optimizer='adam', loss='mse', metrics=['mae'])
+modelo.compile(optimizer='adam', loss='mae', metrics=['mae'])
 
 # Paso 3: Entrenamiento del Modelo
-modelo.fit(datos_entrenamiento, etiquetas_entrenamiento, epochs=100)
+#modelo.fit(datos_entrenamiento, etiquetas_entrenamiento, epochs=100)
 
 # Paso 4: Evaluación del Modelo
 # Aquí deberías dividir tus datos en entrenamiento y prueba para evaluar el modelo adecuadamente
-loss, mae = modelo.evaluate(datos_entrenamiento, etiquetas_entrenamiento)
+#loss, mae = modelo.evaluate(datos_entrenamiento, etiquetas_entrenamiento)
 print("Pérdida del modelo:", loss)
 print("Error absoluto medio:", mae)
